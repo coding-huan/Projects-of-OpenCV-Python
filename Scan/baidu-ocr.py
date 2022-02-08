039c789e52799964da128c7d0855ddf316df2fe4
Mason <164642778@qq.com>
2022-02-09 00:52:55 +0800

    Delete sensitive data

--- Scan/baidu-ocr.py
+++ Scan/baidu-ocr.py
@@ -1,9 +1,9 @@
 from aip import AipOcr
 
-# 调用baidu-ocr接口
-api_key = 'y54FBquGQL7zkHjKwzOl2gTP'
-app_id = '25578790'
-secret_key = 'eeDTMB756FHp5gsxTs1rgUY7yUjL7FOl'
+# 调用baidu-ocr接口，使用你自己申请的
+api_key = ''
+app_id = ''
+secret_key = ''
 options = {"language_type": "CHN_ENG", "detect_direction": "true", "detect_language": "true"}
 
 client = AipOcr(app_id, api_key, secret_key)
